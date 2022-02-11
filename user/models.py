834d470f62318aa88aa28db2a93d6f548cb3ce11
JoaoFiorelli <joao.fiorelli.13@gmail.com>
2022-02-11 18:02:59 -0300

    Criando model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
+from mailbox import NotEmptyError
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    """Model para armazenar dados básicos dos usuários"""
+
+    name = models.CharField(max_length=255) 
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
