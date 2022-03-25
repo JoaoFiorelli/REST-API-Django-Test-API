e19a15b8861ebba9624a4685a502d26e84329188
JoaoFiorelli <joao.fiorelli.13@gmail.com>
2022-03-25 16:15:38 -0300

    Modificando models e settings para autenticação

--- app_project/settings.py
+++ app_project/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'user',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "user.UserProfile"
--- user/models.py
+++ user/models.py
@@ -1,10 +1,34 @@
-from mailbox import NotEmptyError
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 
-class UserProfile(models.Model):
+class UserProfileManager(BaseUserManager):
+    """Manager de UserProfile"""
+
+    def create_user(self, name, email, password=None):
+        """Cria um novo perfil de usuário"""
+
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(name=name, email=email)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Model para armazenar dados básicos dos usuários"""
 
     name = models.CharField(max_length=255) 
     email = models.EmailField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
+    
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
