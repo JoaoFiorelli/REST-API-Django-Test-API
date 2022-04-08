5ba48bd8ce1726a21679ee9b6c8c0157ad1a0e90
JoaoFiorelli <joao.fiorelli.13@gmail.com>
2022-04-08 19:07:07 -0300

    Login funcionando

--- user/models.py
+++ user/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
@@ -21,12 +22,25 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, name, email, password):
+        """Cria um novo super usuário"""
+
+        user = self.create_user(name, email, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Model para armazenar dados básicos dos usuários"""
 
     name = models.CharField(max_length=255) 
     email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     
     objects = UserProfileManager()
 
